50aaf466b69944988e94f6dab67e5cd178b83d9c
GAURANG-21 <gaurang2621@gmail.com>
2024-01-27 17:13:59 +0530

    Protecting Profile Route

--- Protected-Routes/src/App.jsx
+++ Protected-Routes/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import data from './components/index'
+import { useSelector } from 'react-redux'
 
 function App() {
+  // const {isAuthenticated} = useSelector((state)=> { return {root:state.root}})
+  const {isAuthenticated} = useSelector((state)=> state.root)
   return (
     <>
       <Router>
       <data.Navbar/>
         <Routes>
           <Route path='/' element={<data.Home/>}/>
-          <Route path='/admin/profile' element={<data.Profile/>}/>
+          <Route path='/admin/profile' element={isAuthenticated?<data.Profile/>:<data.Login/>}/>
           <Route path='/dashboard' element={<data.Dashboard/>}/>
           <Route path='/login' element={<data.Login/>}/>
           <Route path='/logout' element={<data.Logout/>}/>
